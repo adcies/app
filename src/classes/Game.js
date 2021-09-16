c2c236ec422d7cc59ab30f31ce46ab52b459a0ff
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-09-16 23:06:53 +0200

    add click on tiles feature with reavealing animations, start building main logic

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -1,13 +1,58 @@
 import '../sass/Game.scss';
+import images from '../images';
 
 class Game {
   constructor() {
     this.firstScreen = document.querySelector('.first-screen');
     this.gameElement = document.querySelector('.game');
     this.playButton = document.querySelector('.first-screen__play');
+    this.tiles = [...document.querySelectorAll('.game__tile')];
+    this.images = [];
+    this.tilesClicked = [];
+    this.tilesGuessed = [];
+  }
+
+  #disableTilesClick() {
+    this.tiles.forEach((tile) => (tile.style.pointerEvents = 'none'));
+  }
+
+  #drawImages() {
+    const doubleImages = [...images, ...images];
+    while (doubleImages.length) {
+      const index = Math.floor(Math.random() * doubleImages.length);
+      this.images.push(doubleImages.splice(index, 1));
+    }
+  }
+
+  #checkIfToCompare() {}
+
+  #handleClickOnTile(e, index) {
+    this.tilesClicked.push(e.target);
+    e.target.classList.add('game__tile--active');
+    e.target.style.pointerEvents = 'none';
+    e.target.style.animation = 'rotateTile 0.5s linear 0s 1 both';
+    setTimeout(() => {
+      e.target.style.backgroundImage = `url(${this.images[index]})`;
+      setTimeout(() => {
+        this.#checkIfToCompare();
+      }, 250);
+    }, 250);
+  }
+  #addClickListenerToTiles() {
+    this.tiles.forEach((tile, index) => {
+      tile.addEventListener('click', (e) =>
+        this.#handleClickOnTile.call(this, e, index)
+      );
+    });
+  }
+
+  #startGame() {
+    this.#drawImages();
+    this.#addClickListenerToTiles();
   }
 
   #showBoard() {
+    this.#startGame();
     this.firstScreen.classList.add('first-screen--hidden');
     this.gameElement.classList.add('game--active');
     this.playButton.disabled = true;
---
+++ src/images.js
@@ -0,0 +1,12 @@
+import img1 from './images/tiles/element1.png';
+import img2 from './images/tiles/element2.png';
+import img3 from './images/tiles/element3.png';
+import img4 from './images/tiles/element4.png';
+import img5 from './images/tiles/element5.png';
+import img6 from './images/tiles/element6.png';
+import img7 from './images/tiles/element7.png';
+import img8 from './images/tiles/element8.png';
+import img9 from './images/tiles/element9.png';
+import img10 from './images/tiles/element10.png';
+
+export default [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
